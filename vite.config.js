0f9e6f23385fc64bfd248bfa3d6b12d2ca579c3c
Mahesh3000 <mahesh.sivngi@gmail.com>
2025-04-08 00:54:34 -0500

    config file updated

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    allowedHosts: ["maheshsivangi.tech", "localhost"], // Add allowed hosts here
+  },
 });
